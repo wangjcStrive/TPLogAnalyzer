611c2b70bd3081c77c0063779001fa847309b636
wangjc_desktop <wangjc857@gmail.com>
2021-07-21 16:22:22 +0800

    update

---
+++ TPLogAnalyzer/ExcelWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI;
+using NPOI.SS.UserModel;
+
+namespace TPLogAnalyzer
+{
+    class ExcelWriter
+    {
+        public ExcelWriter(string excelName, string filePath)
+        {
+        }
+    }
+}
